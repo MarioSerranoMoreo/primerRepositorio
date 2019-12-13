c9ea02540376c2233c35b0efe07947cf3a8daaef
DAM_1 <DAM_1@PB03>
2019-12-13 19:44:31 +0100

    resta

--- src/github/pruebaGithub.java
+++ src/github/pruebaGithub.java
@@ -4,9 +4,14 @@ public class pruebaGithub {
 		int numero1=1;
 		int numero2=2;
 		suma(numero1,numero2);
+		resta(numero1,numero2);
 	}
 	public static void suma(int numero1,int numero2) {
 		int resultado=numero1+numero2;
+		System.out.println(resultado);
+	}
+	public static void resta(int numero1,int numero2) {
+		int resultado=numero1-numero2;
 		System.out.println(resultado);
 	}
 
