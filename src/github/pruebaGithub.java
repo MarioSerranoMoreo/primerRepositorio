7f0da2f8bd5da32099d40725f7ba5be23c084bb1
DAM_1 <DAM_1@PB03>
2019-12-13 19:42:04 +0100

    suma

--- src/github/pruebaGithub.java
+++ src/github/pruebaGithub.java
@@ -1,7 +1,13 @@
 package github;
 public class pruebaGithub {
 	public static void main(String[] args) {
-
+		int numero1=1;
+		int numero2=2;
+		suma(numero1,numero2);
+	}
+	public static void suma(int numero1,int numero2) {
+		int resultado=numero1+numero2;
+		System.out.println(resultado);
 	}
 
 }
